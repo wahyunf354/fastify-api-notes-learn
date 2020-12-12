e0e28d62fa4f4b01e93d3af8239f7c48eddb4e4b
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-12-12 12:31:50 +0700

    fix part 8

--- routes/notes/index.js
+++ routes/notes/index.js
@@ -1,119 +1,125 @@
-'use strict'
+"use strict";
+const NotesDAL = require("./notesDAL.js");
 
 module.exports = async function (fastify, opts) {
-  
+  const notesDAL = NotesDAL(fastify.db);
+
   fastify.route({
-    method: 'GET',
-    url: '/',
+    method: "GET",
+    url: "/",
     schema: {
-      tags: ['Notes'],
-      description: 'Get all note',
+      tags: ["Notes"],
+      description: "Get all note",
       response: {
         200: {
-          type: 'array',
+          type: "array",
           items: {
-            type: 'object',
+            type: "object",
             properties: {
-              id: { type: 'string', description: 'unique a value' },
-              title: { type: 'string' },
-              body: { type: 'string', description: 'Main content from note' }
-            }
-          }
-        }
-      }
+              id: { type: "string", description: "unique a value" },
+              title: { type: "string" },
+              body: { type: "string", description: "Main content from note" },
+            },
+          },
+        },
+      },
     },
     handler: async (request, reply) => {
-      return []
-    }
-  })
+      return [];
+    },
+  });
 
   fastify.route({
-    method: 'POST',
-    url: '/',
+    method: "POST",
+    url: "/",
     schema: {
-      tags: ['Notes'],
-      description: 'Create a note',
+      tags: ["Notes"],
+      description: "Create a note",
       body: {
-        type: 'object',
-        required: ['title', 'body'],
+        type: "object",
+        required: ["title", "body"],
         properties: {
-          title: { type: 'string' },
-          body: { type: 'string' }
-        }
+          title: { type: "string" },
+          body: { type: "string" },
+        },
       },
       response: {
         200: {
-          type: 'object',
+          type: "object",
+          required: ["id", "title", "body"],
           properties: {
-            id: { type: 'number', description: 'unique a value' },
-            title: { type: 'string' },
-            body: { type: 'string', description: 'Main content from note' }
-          }
-        }
-      }
+            id: { type: "number", description: "unique a value" },
+            title: { type: "string" },
+            body: { type: "string", description: "Main content from note" },
+          },
+        },
+      },
     },
     handler: async (request, reply) => {
-      return []
-    }
-  })
+      const { title, body } = request.body;
+
+      const newNotes = await notesDAL.createNote(title, body);
+
+      return newNotes;
+    },
+  });
 
   fastify.route({
-    method: 'PUT',
-    url: '/:id',
+    method: "PUT",
+    url: "/:id",
     schema: {
-      tags: ['Notes'],
-      description: 'Update a note',
-      params: { 
-        type: 'object',
-        required: ['id'],
+      tags: ["Notes"],
+      description: "Update a note",
+      params: {
+        type: "object",
+        required: ["id"],
         properties: {
-          id: { type: 'number' }
-        }
+          id: { type: "number" },
+        },
       },
       body: {
-        type: 'object',
-        required: ['title', 'body'],
+        type: "object",
+        required: ["title", "body"],
         properties: {
-          title: { type: 'string' },
-          body: { type: 'string' }
-        }
+          title: { type: "string" },
+          body: { type: "string" },
+        },
       },
       response: {
         200: {
-          type: 'object',
+          type: "object",
           properties: {
-            id: { type: 'number', description: 'unique a value' },
-            title: { type: 'string' },
-            body: { type: 'string', description: 'Main content from note' }
-          }
-        }
-      }
+            id: { type: "number", description: "unique a value" },
+            title: { type: "string" },
+            body: { type: "string", description: "Main content from note" },
+          },
+        },
+      },
     },
     handler: async (request, reply) => {
-      return []
-    }
-  })
+      return [];
+    },
+  });
 
   fastify.route({
-    method: 'DELETE',
-    url: '/:id',
+    method: "DELETE",
+    url: "/:id",
     schema: {
-      tags: ['Notes'],
-      description: 'Delete a note - WARNING - PERMANEND',
-      params: { 
-        type: 'object',
-        required: ['id'],
+      tags: ["Notes"],
+      description: "Delete a note - WARNING - PERMANEND",
+      params: {
+        type: "object",
+        required: ["id"],
         properties: {
-          id: { type: 'number' }
-        }
+          id: { type: "number" },
+        },
       },
       response: {
-        204: { type: 'string', default: 'No Content' }
-      }
+        204: { type: "string", default: "No Content" },
+      },
     },
     handler: async (request, reply) => {
-      return 
-    }
-  })
-
-}
+      return;
+    },
+  });
+};
---
+++ routes/notes/notesDAL.js
@@ -0,0 +1,16 @@
+const NotesDAL = (db) => {
+  const createNote = async (title, body) => {
+    const {
+      id,
+    } = await db.one(
+      "INSERT INTO notes (title, body) VALUES ($1, $2) RETURNING id",
+      [title, body]
+    );
+
+    return { id, title, body };
+  };
+
+  return { createNote };
+};
+
+module.exports = NotesDAL;
